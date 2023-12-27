cd0c5dc333874424ceb9a40f3a5ba7019e9f95b1
BrianBontempi <brian.bont54@gmail.com>
2023-12-27 16:29:03 +0100

    add element in html and take in js + add random dice logic

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log("JS OK")
+
+const button = document.getElementById("button");
+const resultElement = document.getElementById("result");
+
+// ! randomizzo i due dadi 
+
+const playerNumber = Math.floor(Math.random() * 6) + 1;
+const cpuNumber = Math.floor(Math.random() * 6) + 1;
+
+console.log( playerNumber, cpuNumber)
