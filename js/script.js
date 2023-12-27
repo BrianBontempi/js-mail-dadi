0034db2fc8a5eca3f518d638ab2e490b8dbb01a6
BrianBontempi <brian.bont54@gmail.com>
2023-12-27 16:44:00 +0100

    complete exercise

--- js/script.js
+++ js/script.js
@@ -3,9 +3,30 @@ console.log("JS OK")
 const button = document.getElementById("button");
 const resultElement = document.getElementById("result");
 
-// ! randomizzo i due dadi 
+
+button.addEventListener("click", function(){
+    // ! randomizzo i due dadi 
 
 const playerNumber = Math.floor(Math.random() * 6) + 1;
 const cpuNumber = Math.floor(Math.random() * 6) + 1;
 
 console.log( playerNumber, cpuNumber)
+
+// ! chi ha vinto?
+
+let resultMessage = "Pareggio";
+
+if(playerNumber > cpuNumber){
+    resultMessage = "ha vinto il player";
+} else if(playerNumber < cpuNumber){
+    resultMessage = "ha vinto il computer";
+}
+
+const result = `
+<p>Risultato giocatore: ${playerNumber}</p>
+<p>Risultato cpu: ${cpuNumber}</p>
+<p><strong>${resultMessage}</strong></p>
+`;
+
+resultElement.innerHTML = result;
+})
